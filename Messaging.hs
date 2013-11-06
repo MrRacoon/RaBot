76b13911fd239d5b8add3a291ec1c19da783dd20
Erik Sutherland <esutherland@live.com>
2013-11-06 00:23:10 -0800

    added a basic module to parse IRC lines

---
+++ Messaging.hs
@@ -0,0 +1,25 @@
+module Messaging where
+
+import Text.Regex.TDFA
+
+data Message = UnknownLine String
+             | IsPING String
+             | IsPRIVMSG { nick :: String
+                         , user :: String
+                         , host :: String
+                         , chan :: String
+                         , mess :: String }
+    deriving Show
+
+parse :: String -> Message
+parse x
+    | x =~ "PING :"  = let (_,_,a)       = x =~ "PING :"    :: (String, String, String)
+                       in IsPING a
+    | x =~ "PRIVMSG" = let (a,_,message) = x =~ " :"        :: (String, String, String)
+                           (b,_,channel) = a =~ " PRIVMSG " :: (String, String, String)
+                           (c,_,source)  = b =~ "@"         :: (String, String, String)
+                           (d,_,usr)     = c =~ "!"         :: (String, String, String)
+                           nic           = tail d
+                       in IsPRIVMSG nic usr source channel message
+    | otherwise      = UnknownLine x
+
