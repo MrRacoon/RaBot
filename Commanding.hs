091b9421fdf7507ccd96423935dd70255e316598
Erik Sutherland <esutherland@live.com>
2013-12-16 00:51:59 -0800

    Beautification for command code

--- Commanding.hs
+++ Commanding.hs
@@ -2,12 +2,34 @@ module Commanding where
 
 -- ------------------------------------------------------------------------------------------------------------------
 
+import Control.Monad.Trans.State(get,put)
+import Control.Monad.IO.Class(liftIO)
 import Data.List(partition)
 import Messaging(IRC(..))
 import Text.Regex.TDFA((=~))
 import Types
 
 
+botStating = do
+  bs <- get
+  io $ putStrLn $ nickname bs
+  return ()
+
+io :: IO a -> Bot a
+io = liftIO
+
+makeCommands mes = do
+    bs <- get
+    let always = [SayToTerm (show mes)]
+    return $ always ++ case mes of
+       (PING h)            -> [SayToServer Raw "" ("PONG :"++h), SayToTerm ("PONGED: "++h)]
+       (PRIVMSG a b c d e) -> checkCannonRequest d $ concatMap (tryCommand bs mes) $ commands bs
+       (JOIN a b c d)      -> [UserAdd d a]
+       (PART a b c d)      -> [UserPart d a]
+       (QUIT a b c d)      -> [UserQuit a]
+       (SERV a b c d)      -> interpretServ mes
+       (NICK a b c d)      -> [UserNick a d]
+       _        -> []
 
 --parseCommands :: Message -> [Command] -> Maybe [BotAction]
 parseCommands :: BotState -> Message -> [Command] -> [BotAction]
@@ -59,44 +81,50 @@ checkAcl message acl =
 -- ------------------------------------------------------------------------------------------------------------------
 
 resolveArg :: Message -> Argument -> String
-resolveArg _       NULL              = []
-resolveArg ms (Literal s)       = s
-resolveArg ms (WordAfter r)     = let (_,_,a) = (mess ms) =~ r :: (String, String, String)
-                                        in head $ words a
-resolveArg ms (AllWordsAfter r) = let (_,_,a) = (mess ms) =~ r :: (String, String, String)
-                                        in drop 1 a
-resolveArg ms Nickname          = nick ms
-resolveArg ms Username          = user ms
-resolveArg ms FirstChannel      = (mess ms) =~ "#[^ ]*" :: String
-resolveArg ms Channel           = chan ms
-resolveArg ms Hostname          = host ms
-resolveArg ms WholeMessage      = mess ms
-resolveArg ms AllFields         = show ms
-resolveArg ms (KarmaUP a)       = (++"++") $ resolveArg ms a
-resolveArg ms (KarmaDOWN a)     = (++"--") $ resolveArg ms a
-resolveArg _  SourceUrl         = "https://github.com/MrRacoon/RaBot.git"
+resolveArg mes arg =
+    case arg of
+      NULL            -> []
+      Literal s       -> s
+      Nickname        -> nick mes
+      Username        -> user mes
+      FirstChannel    -> (mess mes) =~ "#[^ ]*" :: String
+      Channel         -> chan mes
+      Hostname        -> host mes
+      WholeMessage    -> mess mes
+      AllFields       -> show mes
+      KarmaUP a       -> (++"++") $ resolveArg mes a
+      KarmaDOWN a     -> (++"--") $ resolveArg mes a
+      WordAfter r     -> let (_,_,a) = (mess mes) =~ r :: (String, String, String)
+                             in head $ words a
+      AllWordsAfter r -> let (_,_,a) = (mess mes) =~ r :: (String, String, String)
+                             in drop 1 a
+      SourceUrl       -> "https://github.com/MrRacoon/RaBot.git"
 
 -- ------------------------------------------------------------------------------------------------------------------
 
 makeAction :: Message -> C_Action -> BotAction
-makeAction message (Respond rt args dest)       = SayToServer rt (makeDestination message dest) (unwords $ map (resolveArg message) args)
-makeAction message  ReloadCommands              = Reload (chan message)
-makeAction message (LogToFile file args)        = Log (map (resolveArg message) file) (concatMap (resolveArg message) args)
-makeAction message LoadCannons                  = CannonRequest
-makeAction message CheckCannons                 = ShowPayload (chan message)
-makeAction message FireCannons                  = FirePayload
-makeAction message HelpCommandList              = DisplayHelp message 1 False
-makeAction message HelpCommandListAll           = DisplayHelp message 1 True
-makeAction message HelpUsageList                = DisplayHelp message 2 False
-makeAction message HelpUsageListAll             = DisplayHelp message 2 True
-makeAction message HelpDescriptionList          = DisplayHelp message 3 False
-makeAction message HelpDescriptionListAll       = DisplayHelp message 3 True
-makeAction message ShowCurrentUsers             = ShowUsers (chan message)
-makeAction message (RunScript bin args dest)    = Script bin (concatMap words $ map (resolveArg message) args) (makeDestination message dest)
+makeAction mes act =
+    case act of
+      Respond rt args dest    -> SayToServer rt (makeDestination mes dest) (unwords $ map (resolveArg mes) args)
+      ReloadCommands          -> Reload (chan mes)
+      LogToFile file args     -> Log (map (resolveArg mes) file) (concatMap (resolveArg mes) args)
+      LoadCannons             -> CannonRequest
+      CheckCannons            -> ShowPayload (chan mes)
+      FireCannons             -> FirePayload
+      HelpCommandList         -> DisplayHelp mes 1 False
+      HelpCommandListAll      -> DisplayHelp mes 1 True
+      HelpUsageList           -> DisplayHelp mes 2 False
+      HelpUsageListAll        -> DisplayHelp mes 2 True
+      HelpDescriptionList     -> DisplayHelp mes 3 False
+      HelpDescriptionListAll  -> DisplayHelp mes 3 True
+      ShowCurrentUsers        -> ShowUsers (chan mes)
+      RunScript bin args dest -> Script bin (concatMap words $ map (resolveArg mes) args) (makeDestination mes dest)
 
-loadable (SayToServer Privmsg _ _) = True
-loadable (SayToTerm _)             = True
-loadable _                         = False
+loadable com =
+    case com of
+      SayToServer Privmsg _ _ -> True
+      SayToTerm _             -> True
+      _                       -> False
 
 checkCannonRequest chan actions
       | CannonRequest `elem` actions    = let (load,rest) = partition loadable actions in (LoadPayload load) : rest 
@@ -104,27 +132,34 @@ checkCannonRequest chan actions
 
 
 makeDestination :: Message -> Destination -> String
-makeDestination message To_Current     = chan message
-makeDestination _       To_Server      = []
-makeDestination _       (To_Channel s) = s
+makeDestination message dest =
+    case dest of
+      To_Current     -> chan message
+      To_Server      -> []
+      To_Channel s   -> s
 
 -- ------------------------------------------------------------------------------------------------------------------
 
 checkState :: Bool -> C_State -> Bool
-checkState _     Never   = False
-checkState _     Always  = True
-checkState True  Active  = True
-checkState False Passive = True
-checkState _     _       = False
+checkState mst st =
+    case (mst,st) of
+      (_    , Never  ) -> False
+      (_    , Always ) -> True
+      (True , Active ) -> True
+      (False, Passive) -> True
+      _                -> False
 
 -- ------------------------------------------------------------------------------------------------------------------
 
-trig :: C_Trigger -> String -> Bool
-trig EmptyMessage      [] = True
-trig _                 [] = False
-trig AllMessages       m  = True
-trig (FirstWord w)     m  = (w==) $ head $ words m
-trig (WordPresent w)   m  = elem w $ words m
-trig (EntireMessage r) m  = r == m
-trig (FollowedBy a b)  m  = m =~ (" ?"++a++" "++b++"( |$)") :: Bool
-trig _                 _  = False
+trig EmptyMessage [] = True
+trig trigger mes =
+    case trigger of
+      AllMessages     -> True
+      FirstWord x     -> (x==) $ head $ words mes
+      WordPresent x   -> x `elem` words mes
+      EntireMessage x -> x == mes
+      FollowedBy x y  -> mes =~ (" ?"++x++" "++y++"( |$)") :: Bool
+      _               -> False
+
+
+
