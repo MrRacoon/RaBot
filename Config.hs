13cc05d51e81fc0c28fb49d83700a4658e39f392
Erik Sutherland <esutherland@live.com>
2013-12-31 01:27:42 -0800

    prepend and append command line options for nickname

--- Config.hs
+++ Config.hs
@@ -41,30 +41,34 @@ makeConfig = do
   writeFile "InitialConfig" $ show config
   return config
 
-resolveArguments config []                     = config
-resolveArguments config ("-n":arg:xs)          = resolveArguments (config { bot_nickname    = arg }) xs
-resolveArguments config ("--nick":arg:xs)      = resolveArguments (config { bot_nickname    = arg }) xs
-resolveArguments config ("-a":arg:xs)          = resolveArguments (config { bot_Char        = arg }) xs
-resolveArguments config ("--att":arg:xs)       = resolveArguments (config { bot_Char        = arg }) xs
-resolveArguments config ("-s":arg:xs)          = resolveArguments (config { bot_server      = arg }) xs
-resolveArguments config ("--server":arg:xs)    = resolveArguments (config { bot_server      = arg }) xs
-resolveArguments config ("-p":arg:xs)          = resolveArguments (config { bot_port        = arg }) xs
-resolveArguments config ("--port":arg:xs)      = resolveArguments (config { bot_port        = arg }) xs
-resolveArguments config ("-c":arg:xs)          = resolveArguments (config { bot_chans       = bot_chans config ++ [arg] }) xs
-resolveArguments config ("--channel":arg:xs)   = resolveArguments (config { bot_chans       = bot_chans config ++ [arg] }) xs
-resolveArguments config ("-C":arg:xs)          = resolveArguments (config { bot_commandDir  = arg }) xs
-resolveArguments config ("--command":arg:xs)   = resolveArguments (config { bot_commandDir  = arg }) xs
-resolveArguments config ("-L":arg:xs)          = resolveArguments (config { bot_logDir      = arg }) xs
-resolveArguments config ("--log":arg:xs)       = resolveArguments (config { bot_logDir      = arg }) xs
-resolveArguments config ("-S":arg:xs)          = resolveArguments (config { bot_scriptDir   = arg }) xs
-resolveArguments config ("--script":arg:xs)    = resolveArguments (config { bot_scriptDir   = arg }) xs
-resolveArguments config ("-N":arg:xs)          = resolveArguments (config { bot_ownerNick   = arg }) xs
-resolveArguments config ("--ownerNick":arg:xs) = resolveArguments (config { bot_ownerNick   = arg }) xs
-resolveArguments config ("-U":arg:xs)          = resolveArguments (config { bot_ownerUser   = arg }) xs
-resolveArguments config ("--ownerUser":arg:xs) = resolveArguments (config { bot_ownerUser   = arg }) xs
-resolveArguments config ("-h":_)               = error programUsage
-resolveArguments config ("--help":_)           = error programUsage
-resolveArguments _       (x:xs)                = error ("Error: Incorrect Argument: "++x++"\n"++programUsage)
+resolveArguments config []                        = config
+resolveArguments config ("-n":arg:xs)             = resolveArguments (config { bot_nickname    = arg }) xs
+resolveArguments config ("--nick":arg:xs)         = resolveArguments (config { bot_nickname    = arg }) xs
+resolveArguments config ("-pn":arg:xs)            = resolveArguments (config { bot_nickname    = arg++(bot_nickname config) }) xs
+resolveArguments config ("--prepend-nick":arg:xs) = resolveArguments (config { bot_nickname    = arg++(bot_nickname config) }) xs
+resolveArguments config ("-an":arg:xs)            = resolveArguments (config { bot_nickname    = (bot_nickname config)++arg }) xs
+resolveArguments config ("--append-nick":arg:xs)  = resolveArguments (config { bot_nickname    = (bot_nickname config)++arg }) xs
+resolveArguments config ("-a":arg:xs)             = resolveArguments (config { bot_Char        = arg }) xs
+resolveArguments config ("--att":arg:xs)          = resolveArguments (config { bot_Char        = arg }) xs
+resolveArguments config ("-s":arg:xs)             = resolveArguments (config { bot_server      = arg }) xs
+resolveArguments config ("--server":arg:xs)       = resolveArguments (config { bot_server      = arg }) xs
+resolveArguments config ("-p":arg:xs)             = resolveArguments (config { bot_port        = arg }) xs
+resolveArguments config ("--port":arg:xs)         = resolveArguments (config { bot_port        = arg }) xs
+resolveArguments config ("-c":arg:xs)             = resolveArguments (config { bot_chans       = bot_chans config ++ [arg] }) xs
+resolveArguments config ("--channel":arg:xs)      = resolveArguments (config { bot_chans       = bot_chans config ++ [arg] }) xs
+resolveArguments config ("-C":arg:xs)             = resolveArguments (config { bot_commandDir  = arg }) xs
+resolveArguments config ("--command":arg:xs)      = resolveArguments (config { bot_commandDir  = arg }) xs
+resolveArguments config ("-L":arg:xs)             = resolveArguments (config { bot_logDir      = arg }) xs
+resolveArguments config ("--log":arg:xs)          = resolveArguments (config { bot_logDir      = arg }) xs
+resolveArguments config ("-S":arg:xs)             = resolveArguments (config { bot_scriptDir   = arg }) xs
+resolveArguments config ("--script":arg:xs)       = resolveArguments (config { bot_scriptDir   = arg }) xs
+resolveArguments config ("-N":arg:xs)             = resolveArguments (config { bot_ownerNick   = arg }) xs
+resolveArguments config ("--ownerNick":arg:xs)    = resolveArguments (config { bot_ownerNick   = arg }) xs
+resolveArguments config ("-U":arg:xs)             = resolveArguments (config { bot_ownerUser   = arg }) xs
+resolveArguments config ("--ownerUser":arg:xs)    = resolveArguments (config { bot_ownerUser   = arg }) xs
+resolveArguments config ("-h":_)                  = error programUsage
+resolveArguments config ("--help":_)              = error programUsage
+resolveArguments _       (x:xs)                   = error ("Error: Incorrect Argument: "++x++"\n"++programUsage)
 
 
 programUsage = "usage: rabot [Configuration Parameters]\n\n"
